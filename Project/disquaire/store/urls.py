4af52417a68722a658670d061857bce081142b88
h120ayat <93580480+h120ayat@users.noreply.github.com>
2022-01-22 20:55:54 +0000

    commits

---
+++ Project/disquaire/store/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.conf.urls import include
+
+
+from . import views 
+
+urlpatterns = [
+    # url('', views.index),
+    url('hayat/', views.index),
+
+]
---
+++ Project/disquaire/store/views.py
@@ -0,0 +1,14 @@
+# from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def index(request):
+	message = "Salut tout le monde"
+	return HttpResponse(message)
+
+
+
+def identite(request):
+	me = "hello haat"
+	return HttpResponse(me)
