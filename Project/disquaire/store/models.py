5ba1ebcd0fd6043f62dea04f8a7f48d074e797bd
h120ayat <93580480+h120ayat@users.noreply.github.com>
2022-01-23 10:24:20 +0000

    Update models.py

--- Project/disquaire/store/models.py
+++ Project/disquaire/store/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+ARTISTS = {
+'francis-cabriel' : {'name': 'Francis Cabriel'},
+'lej': {'name': 'Elijay'},
+'rosana': {'name': 'Rosana'},
+'maria': {'name': 'Maria Dolores Pradera'},
+}
+
+
+ALBUMS = [
+{'name': 'Sarbacane', 'artists': [ARTISTS['francis-cabrel']]},
+{'name': 'la dalle', 'artists': [ARTISTS['lej']]},
+{'name': 'Luna Nueva', 'artists': [ARTISTS['rosana'], ARTISTS['maria']]}
+]
